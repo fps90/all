7c0249aaf38ad9bc4563e07752bc84501bce61f9
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-05-30 20:14:40 +0300

    Update start.py

--- YukkiMusic/plugins/bot/start.py
+++ YukkiMusic/plugins/bot/start.py
@@ -24,7 +24,7 @@ async def start_(c: Client, message: Message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("⦗ اوامر البوت ⦘", callback_data="command_list")
+                    InlineKeyboardButton("⦗ اوامر البوت ⦘", callback_data="user_command")
                 ],[
                     InlineKeyboardButton("⦗ قناة السورس ⦘", url=SUPPORT_CHANNEL),
                     InlineKeyboardButton("⦗ قناة التحديثات ⦘", url=SUPPORT_GROUP),
@@ -65,16 +65,57 @@ async def start_set(_, query: CallbackQuery):
     )
 
 
-@app.on_callback_query(filters.regex("user_guide"))
+@app.on_callback_query(filters.regex("command_list"))
+async def commands_set(_, query: CallbackQuery):
+    user_id = query.from_user.id
+    await query.answer("👍🏻قائمة الاوامر")
+    await query.edit_message_text(
+        f"""- تابع الازرار في الاسفل ↓
+
+يمديك تشوف كل اوامر البوت عن طريق زر اوامر البوت""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("-› اوامر التشغيل", callback_data="user_command"),
+                ],
+                [
+                    InlineKeyboardButton("-› ࢪرجَۅعَ", callback_data="home_start"),
+                    InlineKeyboardButton("التالي", callback_data="next"),
+                ],
+            ]
+        ),
+    )
+
+@app.on_callback_query(filters.regex("next"))
+async def next_set(_, query: CallbackQuery):
+    await query.answer("التالي")
+    await query.edit_message_text(
+        "يرجى اختيار نوع الأوامر:",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("⦗ اوامر الادمن ⦘", callback_data="admin_commands"),
+                    InlineKeyboardButton("⦗ اوامر المطور ⦘", callback_data="developer_commands"),
+                ],
+                [
+                    InlineKeyboardButton("-› ࢪرجَۅعَ", callback_data="command_list"),
+                    InlineKeyboardButton("التالي", callback_data="user_command"),
+                ],
+            ]
+        ),
+    )
+
+
+@app.on_callback_query(filters.regex("user_command"))
 async def guide_set(_, query: CallbackQuery):
-    await query.answer("user guide")
+    await query.answer("اوامر التشغيل")
     await query.edit_message_text(
         f"""طريقة التشغيل ، تابع في الاسفل ↓
 
 1-› أولا ، أضفني الى مجموعتك
 2-› بعد ذالك قم برفعي كمشرف واعطائي صلاحيات مثل باقي البشر.
 3-› بعد ذالك اكتب `.تحديث` بيانات البوت
-3-› اضف سيدي ومولاي  في مجموعتك او اكتب `.انضم` لدعوة المساعد
+3-› اضف سيدي ومولاي في مجموعتك او اكتب `.انضم` لدعوة المساعد
 4-› اذ لم تستطيع اضافة المساعد او واجهت مشاكل تحدث مع رئيس الوزراء  .
 
 """,
@@ -88,19 +129,14 @@ async def guide_set(_, query: CallbackQuery):
     )
 
 
-@app.on_callback_query(filters.regex("command_list"))
-async def commands_set(_, query: CallbackQuery):
-    user_id = query.from_user.id
-    await query.answer("👍🏻قائمة الاوامر")
-    await query.edit_message_text(
-        f"""- تابع الازرار في الاسفل ↓
 
-يمديك تشوف كل اوامر البوت عن طريق زر اوامر البوت""",
+@app.on_callback_query(filters.regex("admin_commands"))
+async def guide_set(_, query: CallbackQuery):
+    await query.answer("اوامر الادمن")
+    await query.edit_message_text(
+        f"""اوامر الادمن""",
         reply_markup=InlineKeyboardMarkup(
             [
-                [
-                    InlineKeyboardButton("-› اوامر البوت", callback_data="user_command"),
-                ],
                 [
                     InlineKeyboardButton("-› ࢪجَۅعَ", callback_data="home_start")
                 ],
@@ -109,28 +145,26 @@ async def commands_set(_, query: CallbackQuery):
     )
 
 
-@app.on_callback_query(filters.regex("user_command"))
-async def user_set(_, query: CallbackQuery):
-    await query.answer("👍🏻اوامر التشغيل")
+
+@app.on_callback_query(filters.regex("developer_commands"))
+async def guide_set(_, query: CallbackQuery):
+    await query.answer("اوامر المطورين")
     await query.edit_message_text(
-        f"""- تابع الاوامر في الاسفل ↓
+        f"""اوامر المطورين ↓
 
--› .شغل - بالرد على ملف صوتي او اسم أغنية
--› .اصعد - لصعود حساب المساعد في المكالمة
--› .انزل - لنزول المساعد من المكالمة
--› .تخطي - لتخطي اغنية في التشغيل
--› .كافي - لايقاف تشغيل جميع الاغاني
--› .اضبط - لضبط صوت حساب المساعد
--› .فيديو - بالرد على مقطع فيديو او اسم فيديو
--› .الانتضار - لرؤية قائمة الانتضار التشغيل
--› .ابحثلي - لبحث عن فيديو من اليوتيوب
--› .بحث - لتحميل اغنية من اليوتيوب
--› .كتم - لكتم صوت المساعد 
--› .بنك - لإضهار بنك البوت
--› .انضم - لدعوة حساب المساعد
+1-› أولا ، أضفني الى مجموعتك
+2-› بعد ذالك قم برفعي كمشرف واعطائي صلاحيات مثل باقي البشر.
+3-› بعد ذالك اكتب `.تحديث` بيانات البوت
+3-› اضف سيدي ومولاي @{me_user.username} في مجموعتك او اكتب `.انضم` لدعوة المساعد
+4-› اذ لم تستطيع اضافة المساعد او واجهت مشاكل تحدث مع رئيس الوزراء  .
 
-. شكراً لقرائتك الاوامر - أتمنى لك يوماً تعيساً 🦴 """,
+""",
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("-› ࢪجَۅعَ", callback_data="command_list")]]
+            [
+                [
+                    InlineKeyboardButton("-› ࢪجَۅعَ", callback_data="home_start")
+                ],
+            ]
         ),
     )
+
