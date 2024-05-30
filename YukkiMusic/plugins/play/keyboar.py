bf916febed7c0a440e4815a9151df890cc3e7bcb
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-05-31 01:40:15 +0300

    Update keyboar.py

--- YukkiMusic/plugins/play/keyboar.py
+++ YukkiMusic/plugins/play/keyboar.py
@@ -9,9 +9,4 @@ keyboard = ReplyKeyboardMarkup([['Button 1', 'Button 2']], resize_keyboard=True)
 
 @app.on_message(filters.command('admin') & filters.user(OWNER))
 def start_(client, message):
-    message.reply_text('لوحة المفاتيح للمطور:', reply_markup=keyboard)
-
-# دالة لاختبار لوحة المفاتيح
-@app.on_message(filters.command('start') & filters.user(OWNER))
-def start_(client, message):
-    message.reply_text('قم بتجربة لوحة المفاتيح:', reply_markup=keyboard)
+    message.reply_text('اهلا بك عزيزي ⦗ المطور الاساسي ⦘ \n – – – – – –', reply_markup=keyboard)
