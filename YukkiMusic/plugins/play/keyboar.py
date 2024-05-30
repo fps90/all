020e88cbf6bb65b604a1947c66282fb4ae80fac8
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-05-31 02:00:48 +0300

    Update keyboar.py

--- YukkiMusic/plugins/play/keyboar.py
+++ YukkiMusic/plugins/play/keyboar.py
@@ -8,6 +8,6 @@ from YukkiMusic.misc import SUDOERS
 # تحديد لوحة المفاتيح
 keyboard = ReplyKeyboardMarkup([['Button 1', 'Button 2']], resize_keyboard=True)
 
-@app_client.on_message(filters.command('start') & ~filters.user(SUDOERS) & ~filters.user(OWNER))
+@app.on_message(filters.command('start') & ~filters.user(SUDOERS) & ~filters.user(OWNER))
 async def start_command(client, message: Message):
     await message.reply_text('لوحة المفاتيح للمطور:', reply_markup=keyboard)
