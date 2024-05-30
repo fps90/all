65345ba20fdcef4ea6865ede0eef98b5a04ea12a
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-05-31 01:59:19 +0300

    Update keyboar.py

--- YukkiMusic/plugins/play/keyboar.py
+++ YukkiMusic/plugins/play/keyboar.py
@@ -3,10 +3,11 @@ from YukkiMusic import app
 from pyrogram import Client, filters
 from pyrogram.types import ReplyKeyboardMarkup, Message
 from config import OWNER
+from YukkiMusic.misc import SUDOERS
 
 # تحديد لوحة المفاتيح
 keyboard = ReplyKeyboardMarkup([['Button 1', 'Button 2']], resize_keyboard=True)
 
-@app.on_message(filters.command('start') & filters.user(OWNER))
+@app_client.on_message(filters.command('start') & ~filters.user(SUDOERS) & ~filters.user(OWNER))
 async def start_command(client, message: Message):
     await message.reply_text('لوحة المفاتيح للمطور:', reply_markup=keyboard)
