b6fe0b642ee1d53ff8d0f149c21f83be9a2b529e
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-05-31 03:19:25 +0300

    Update keyboar.py

--- YukkiMusic/plugins/play/keyboar.py
+++ YukkiMusic/plugins/play/keyboar.py
@@ -6,8 +6,21 @@ from pyrogram.types import ReplyKeyboardMarkup, Message
 from config import OWNER
 
 # تحديد لوحة المفاتيح
-keyboard = ReplyKeyboardMarkup([['Button 1', 'Button 2']], resize_keyboard=True)
+keyboard = ReplyKeyboardMarkup(
+	[('⦗ فتح الكيبورد ⦘'),
+	[('⦗ اعادة تشغيل ⦘'),('⦗ تحديث السورس ⦘'),
+	[('حذف الاعضاء الفيك'),('حذف الجروبات الفيك')],
+	[('الاصدار'),('تحديث السورس'),('سرعه السيرفر')],
+	[('اذاعه للمستخدمين'),('اذاعه للجروبات')],
+	[('اذاعه للمطورين'),('اذاعه للاساسيين'),('اذاعه قنوات')],
+	[('اذاعه للكل'),('توجيه للكل')],
+	[('توجيه للمستخدمين'),('توجيه للجروبات'),('توجيه للقنوات')],
+	[('توجيه للاساسيين'),('توجيه للمطورين')],
+	[('حذف الكيبورد ⚒️')]],
+	resize_keyboard=True,
+	one_time_keyboard=False
+	)
 
-@app.on_message(command(["كيبورد", "الكيبورد"]) & filters.private & filters.user(OWNER))
+@app.on_message(command(["⦗ فتح الكيبورد ⦘", "/start"]) & filters.private & filters.user(OWNER))
 async def start_or_help_command(client, message: Message):
-    await message.reply_text('اهلأ بك عزيزي ⦗ المطور الاساسي ⦘ \n – – – – – – \n ⦗ يمكنك التحكم عن طريق الازرار أدناه ⦘ ✓', reply_markup=keyboard)
+    await message.reply_text('اهلأ بك عزيزي ⦗ المطور الاساسي ⦘ \n – – – – – – \n⦗ يمكنك التحكم عن طريق الأزرار أدناه ⦘', reply_markup=keyboard)
